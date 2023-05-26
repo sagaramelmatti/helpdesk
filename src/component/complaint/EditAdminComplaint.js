426844a9f594039e2928fd2e2b258c65f133a611
insmelm <sagar.melmatti@vanderlande.com>
2023-05-26 23:56:25 +0530

    two edit files

---
+++ src/component/complaint/EditAdminComplaint.js
@@ -0,0 +1,196 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Select from "react-select";
+
+import {
+  addComplaints,
+  getUsers,
+  getComplaintById,
+  updateComplaintById,
+  getLocationList,
+} from "../../api";
+import { addComplaintFormConstants } from "../constants";
+import { toast } from "react-toastify";
+import {
+  API_ADMIN_COMPLAINTS
+} from "../../component/constants";
+
+function EditAdminComplaint(props) {
+
+  const navigate = useNavigate();
+
+  const [userList, setUserList] = useState({});
+  const [locationList, setLocationList] = useState({});
+
+  const [addComplaintFormFields, setAddComplaintFormFields] = useState({
+    title: "",
+    description: "",
+    userId: "",
+    status: "",
+    locationId: "",
+  });
+
+  const role = localStorage.getItem("role");
+  const params = useParams();
+
+  useEffect(() => {
+    if (params?.id) {
+      getComplaintById(params?.userId, params?.id).then((res) => {
+        setAddComplaintFormFields({
+          title: res?.data?.title || "",
+          description: res?.data?.description || "",
+          userId: res?.data?.userId || "",
+          status: res?.data?.status || "",
+          locationId: res?.data?.locationId || "",
+        });
+      });
+    }
+    if (role === "ROLE_ADMIN") {
+      getUsers().then((response) => {
+        if (response?.status === 200) {
+          const userListTemp = response?.data?.map((item) => {
+            return { value: item?.id, label: item?.name };
+          });
+          setUserList(userListTemp);
+          return response?.data;
+        }
+      });
+    }
+  }, [params.id, role]);
+
+  useEffect(() => {
+
+    getLocationList().then((response) => {
+      if (response?.status === 200) {
+        const locationListTemp = response?.data?.map((item) => {
+          return { value: item?.id, label: item?.name };
+        });
+        setLocationList(locationListTemp);
+      }
+    });
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    updateComplaintById(API_ADMIN_COMPLAINTS, params?.id, addComplaintFormFields).then(
+      (response) => {
+        if (response?.status === 200) {
+          toast.success("Complaint added successfully");
+            navigate("/admin/complaints");
+        } else {
+          toast.error("Something went wrong, please try again");
+        }
+      }
+    );
+  };
+
+  const showOptionsList = (formFieldKey) => {
+    switch (formFieldKey) {
+      case "userId":
+        return userList;
+      case "locationId":
+        return locationList;
+      default:
+        return "";
+    }
+  };
+
+  const findSelectedOption = (listParam) => {
+    const listParamTemp = showOptionsList(listParam);
+    if (listParamTemp?.length) {
+      return listParamTemp?.find(
+        (list) => list?.value === addComplaintFormFields[listParam]
+      );
+    }
+  };
+
+  const renderFormFields = () => {
+    return addComplaintFormConstants?.map((formField) => {
+      if (formField?.roleList?.includes(role)) {
+        return formField?.type === "text" ? (
+          <div className="col-md-6" key={formField?.key}>
+            <div className="form-group required">
+              <label className="control-label">{formField?.label}</label>
+              <input
+                type={formField?.type}
+                className="form-control"
+                name={formField?.key}
+                value={addComplaintFormFields?.[formField?.key]}
+                onChange={(e) =>
+                  setAddComplaintFormFields({
+                    ...addComplaintFormFields,
+                    [formField?.key]: e.target.value,
+                  })
+                }
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="col-md-6" key={formField?.key}>
+            <div className="form-group required">
+              <label className="control-label">{formField?.label}</label>
+              <Select
+                value={findSelectedOption(formField?.key)}
+                onChange={(e) =>
+                  setAddComplaintFormFields({
+                    ...addComplaintFormFields,
+                    [formField?.key]: e.value,
+                  })
+                }
+                options={showOptionsList(formField?.key)}
+              />
+            </div>
+          </div>
+        );
+      }
+    });
+  };
+
+  return (
+    <>
+      <div className="content-wrapper">
+        <section className="content-header">
+          <h1>Edit Complaint</h1>
+        </section>
+        <section className="content" style={{ minHeight: "500px" }}>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="box">
+                <div className="box-header">
+                  <h3 className="box-title"> Complaint Master</h3>
+                </div>
+                <div className="box-body">
+                  <div className="row">{renderFormFields()}</div>
+                  <div className="row">
+                    <div className="col-md-12 text-center">
+                      <button
+                        style={{ width: "200px", display: "inline-block" }}
+                        type="submit"
+                        className="btn btn-success btn-block btn-flat r-btn"
+                        onClick={(e) => {
+                          handleSubmit(e);
+                        }}
+                        disabled={
+                          !addComplaintFormFields?.title ||
+                          !addComplaintFormFields?.description |
+                            !addComplaintFormFields?.userId ||
+                          !addComplaintFormFields?.locationId
+                        }
+                      >
+                        Update
+                      </button>
+                    </div>
+                    <div className="col-md-6"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default EditAdminComplaint;
